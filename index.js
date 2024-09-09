07ec291026831f063c2bb0bb13983833f47c91ba
Felipe Bucherenick <felipebucherenick@gmail.com>
2024-09-09 19:26:38 -0300

    feat:Add modal to add a new book to the library

--- index.js
+++ index.js
@@ -76,10 +76,68 @@ myLibrary.forEach((book) => {
   listOfBooks.appendChild(displayBook);
 });
 
-const newBook = document.createElement("div");
-const newBookContent = document.createElement("p");
-newBookContent.textContent = "+";
-newBook.appendChild(newBookContent);
-newBook.classList.add("book");
-newBook.classList.add("new-book");
-listOfBooks.appendChild(newBook);
+const createNewBook = document.createElement("div");
+const createNewBookContent = document.createElement("p");
+createNewBookContent.textContent = "+";
+createNewBook.appendChild(createNewBookContent);
+createNewBook.classList.add("book");
+createNewBook.classList.add("new-book");
+listOfBooks.appendChild(createNewBook);
+
+const openAddNewBookModal = () => {
+  const addNewBookModal = document.createElement("div");
+  const addNewBookForm = document.createElement("form");
+
+  const addNewBookModalTitle = document.createElement("h2");
+  addNewBookModalTitle.textContent = "New book";
+  addNewBookForm.appendChild(addNewBookModalTitle);
+
+  const addNewBookTitleLabel = document.createElement("label");
+  addNewBookTitleLabel.textContent = "Title";
+  const addNewBookTitleInput = document.createElement("input");
+  addNewBookForm.appendChild(addNewBookTitleLabel);
+  addNewBookForm.appendChild(addNewBookTitleInput);
+
+  const addNewBookAuthorLabel = document.createElement("label");
+  addNewBookAuthorLabel.textContent = "Author";
+  const addNewBookAuthorInput = document.createElement("input");
+  addNewBookForm.appendChild(addNewBookAuthorLabel);
+  addNewBookForm.appendChild(addNewBookAuthorInput);
+
+  const addNewBookPagesLabel = document.createElement("label");
+  addNewBookPagesLabel.textContent = "Pages";
+  const addNewBookPagesInput = document.createElement("input");
+  addNewBookForm.appendChild(addNewBookPagesLabel);
+  addNewBookForm.appendChild(addNewBookPagesInput);
+
+  const addNewBookIsReadLabel = document.createElement("label");
+  addNewBookIsReadLabel.textContent = "IsRead";
+  const addNewBookIsReadInput = document.createElement("input");
+  addNewBookForm.appendChild(addNewBookIsReadLabel);
+  addNewBookForm.appendChild(addNewBookIsReadInput);
+
+  const addNewBookDone = document.createElement("button");
+  addNewBookDone.textContent = "Done";
+  addNewBookForm.appendChild(addNewBookDone);
+
+  addNewBookModal.appendChild(addNewBookForm);
+  addNewBookModal.classList.add("add-new-book-modal");
+  document.body.appendChild(addNewBookModal);
+
+  addNewBookDone.addEventListener("click", (event) => {
+    event.preventDefault();
+    const newBook = new Book(
+      addNewBookTitleInput.value,
+      addNewBookAuthorInput.value,
+      addNewBookPagesInput.value,
+      addNewBookIsReadInput.value
+    );
+    addBookToLibrary(newBook);
+    addNewBookModal.classList.add("close");
+    console.log(myLibrary);
+  });
+};
+
+createNewBook.addEventListener("click", () => {
+  openAddNewBookModal();
+});
